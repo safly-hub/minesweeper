02404eae6167e3ee66179f5f3b84b80d1c67d501
LUCAS SAFLY <saflycorp@gmail.com>
2021-03-26 10:20:30 -0400

    app jsx.gameboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,99 @@
 import React, { Component } from 'react'
 
-class App extends Component {
+export class App extends Component {
+  state = {
+    board: [
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    ],
+  }
+
   render() {
-    return <div>Hello, World!</div>
+    const boardGame = this.state.board.map((row, rowIndex) => {
+      return row.map((cell, colIndex) => {
+        return <li key={colIndex}></li>
+      })
+    })
+
+    return (
+      <div className="gameboard">
+        <main>
+          <h2>Minesweeper Game</h2>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+          <footer></footer>
+        </main>
+      </div>
+    )
   }
 }
-
-export default App
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.scss'
 import App from './App'
+export App from './src/index.js'
 
 ReactDOM.render(<App />, document.getElementById('root'))
